77a1d0f19c365907e762884d32fff213052e649f
Quegrec <thomas.m.querrec@gmail.com>
2024-04-16 22:18:29 +0200

    Update environment.ts

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     googleClientId: '806850205984-tl6g4p537cdmtj3jlik3srgubmv3onec.apps.googleusercontent.com',
     apiUrl: 'http://localhost:3000',
 };
